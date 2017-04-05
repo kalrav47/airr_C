6e325758155c067d538d058987dcd1c7505f9564
Kalrav Parsana <kalravparsana@gmail.com>
2017-04-05 22:41:09 +0530

    bridgeServer beautified

--- bridgeServer.c
+++ bridgeServer.c
@@ -1,127 +1,104 @@
-//gcc server.c -o server -lpthread  -- for pc
-//~/CHIP-SDK/CHIP-buildroot/output/host/usr/bin/arm-linux-gnueabihf-gcc server.c -o server -lpthread -w   -- for chip
+// Compile for PC : gcc server.c -o server -lpthread
+// Compile for CHIP : ~/CHIP-SDK/CHIP-buildroot/output/host/usr/bin/arm-linux-gnueabihf-gcc server.c -o server -lpthread -w
 
-//General header files
-#include<sys/types.h>
-#include<sys/socket.h>
-#include<stdio.h>
-#include<unistd.h>
-#include<netinet/in.h>
-#include<pthread.h>
-#include<string.h>
-#include<signal.h>
-#include<time.h>
+// Header file
+#include "bridgeServer.h"
 
 
 // Global variables
-int csd=0,csdd=0;
-int socket_write, socket_read,port,portr, ssize,i;
-
+int client_socketid=0;
+int socket_write = 0;
 pthread_t threads;
 
-
-#define MAX 4096
-#define MAX_CMD 20
-#define MAXCONN 1000
-
-void exit_function(char message[MAX])
-{	
-	printf("Exiting server with error : %s\n",message);
-	exit(0);
+void exit_function(char message[])
+{
+    printf("Exiting server with error : %s\n",message);
+    exit(0);
 }
 
-void *start_listen_To_all( void *param)
+void *serveRequest( void *param)
 {
-	int *csdd = (int*)param;
-	
-	int sockfd = 0;
-    	char msg[20];
-    	struct sockaddr_in serv_addr; 
-	time_t rawtime;
-  	struct tm * timeinfo;
+    int *public_client_socketid = (int*)param;
+    int internal_client_socketid = 0;
+    char msg[MAX_CMD];
 
- 
-    if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    // create socket to internal server
+    struct sockaddr_in serv_addr;
+
+    if((internal_client_socketid = socket(AF_INET, SOCK_STREAM, 0)) < 0)
     {
-        printf("\n Error : Could not create socket \n");
-        return 1;
-    } 
+        exit_function("Could not create socket");
+    }
 
-    memset(&serv_addr, '0', sizeof(serv_addr)); 
+    memset(&serv_addr, '0', sizeof(serv_addr));
 
     serv_addr.sin_family = AF_INET;
-    serv_addr.sin_port = htons(5000); 
+    serv_addr.sin_port = htons(INTERNAL_SERVER_PORT);
 
-    if(inet_pton(AF_INET,"192.168.0.1", &serv_addr.sin_addr)<=0)
+    if(inet_pton(AF_INET,INTERNAL_SERVER_IP,&serv_addr.sin_addr)<=0)
     {
-        printf("\n inet_pton error occured\n");
-        return 1;
-    } 
+        exit_function("inet_pton error occured");
+    }
 
-    if( connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
+    // connect to internal server
+    if( connect(internal_client_socketid, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
     {
-       printf("\n Error : Connect Failed \n");
-       return 1;
-    } 
-    	
-    	printf("Connected.. and asking now...\n");
-	time ( &rawtime );
- 	timeinfo = localtime ( &rawtime );
-	
-	read(*csdd,msg,20);
+        exit_function("Connect Failed");
+    }
 
-    	write(sockfd,msg,strlen(msg)+1);
-	
-	read(sockfd,msg,20);
-	
-	write(*csdd,msg,strlen(msg)+1);
- 	close(sockfd);
- 	close(*csdd);
+    // read request from public clinet
+    read(*public_client_socketid,msg,MAX_CMD);
+
+    // pass it on to internal server
+    write(internal_client_socketid,msg,MAX_CMD+1);
+
+    // get reply from internal server
+    read(internal_client_socketid,msg,MAX_CMD);
+
+    // reply back to public client
+    write(*public_client_socketid,msg,MAX_CMD+1);
+
+    // close both sockets as not required any more
+    close(internal_client_socketid);
+    close(*public_client_socketid);
 }
 
 
 
-int main(void)
+void main(void)
 {
-	
-	long int count=1,i;
-	struct sockaddr_in saddress,raddress;
-	struct sockaddr_in caddress[MAXCONN]; 
-	
-	char message[MAX];
 
-	socket_write = socket( AF_INET, SOCK_STREAM, 0);
-	if( socket_write == -1)
-	{
-		exit_function("Socket init write");
-	}
+    struct sockaddr_in saddress;
 
-	int on = 1;
-	int ret = setsockopt( socket_write, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on) );
+    socket_write = socket( AF_INET, SOCK_STREAM, 0);
+    if( socket_write == -1)
+    {
+        exit_function("Socket init write");
+    }
 
-	/* bind address to socket */
-	saddress.sin_family = AF_INET;
-	saddress.sin_port = htons(6000);
-	saddress.sin_addr.s_addr = INADDR_ANY;
+    int on = 1;
+    int ret = setsockopt( socket_write, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on) );
 
-	if( bind( socket_write, (struct sockaddr*)&saddress, sizeof(saddress)) == -1)
-	{
-		exit_function("Socket bind write");
-	}
+    // bind address to socket
+    saddress.sin_family = AF_INET;
+    saddress.sin_port = htons(PUBLIC_SERVER_PORT);
+    saddress.sin_addr.s_addr = INADDR_ANY;
 
-	/* mark socket to listen for incoming connections */
-	listen( socket_write, MAXCONN);
-	
-	/* wait for a client connection */
-	ssize = sizeof(caddress[0]);
-	while(1)
-	{
-		csd = accept( socket_write, (struct sockaddr*)NULL,NULL);
-		pthread_create( &threads, NULL,start_listen_To_all, (void*)&csd);
-		//printf("Thread started ..\n");
-		pthread_join(threads,NULL);
-		//printf("Thread finished.. \n");
-	}	
-		pthread_exit(NULL);
-	
+    if( bind( socket_write, (struct sockaddr*)&saddress, sizeof(saddress)) == -1)
+    {
+        exit_function("Socket bind write");
+    }
+
+    // mark socket to listen for incoming connections
+    listen( socket_write, MAXCONN);
+
+    // wait for a client connection
+    while(1)
+    {
+        client_socketid = accept( socket_write, (struct sockaddr*)NULL,NULL);
+        pthread_create( &threads, NULL,serveRequest, (void*)&client_socketid);
+        pthread_join(threads,NULL);
+    }
+
+    pthread_exit(NULL);
 }
---
+++ bridgeServer.h
@@ -0,0 +1,27 @@
+//General header files
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<netinet/in.h>
+#include<pthread.h>
+#include<string.h>
+
+#define MAX 4096
+#define MAX_CMD 20
+#define MAXCONN 1000
+#define INTERNAL_SERVER_IP "192.168.0.1"
+#define INTERNAL_SERVER_PORT 5000
+#define PUBLIC_SERVER_PORT 6000
+
+// Quit program in case of error
+// @ input : exit message
+void exit_function(char[]);
+
+// Serve the request request by mobile app or anything from public.
+// It replies to specific command by sending request to appropriate switch board 
+// and taking reply from it.
+// @ input : cliend socket id
+void *serveRequest( void *);
+
