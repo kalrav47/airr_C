e0d16c6722e97590d560f896ffd4acda12a43d9e
Kalrav Parsana <kalravparsana@gmail.com>
2017-04-06 23:47:27 +0530

    Beautified server.c

--- server.c
+++ server.c
@@ -1,213 +1,188 @@
-//gcc server.c -o server -lpthread  -- for pc
-//~/CHIP-SDK/CHIP-buildroot/output/host/usr/bin/arm-linux-gnueabihf-gcc server.c -o server -lpthread -w   -- for chip
+// Compile for PC : gcc server.c -o server -lpthread
+// Compile for CHIP : ~/CHIP-SDK/CHIP-buildroot/output/host/usr/bin/arm-linux-gnueabihf-gcc server.c -o server -lpthread -w
 
-//General header files
-#include<sys/types.h>
-#include<sys/socket.h>
-#include<stdio.h>
-#include<unistd.h>
-#include<netinet/in.h>
-#include<pthread.h>
-#include<string.h>
-#include<signal.h>
-#include<time.h>
+// Header file
+#include "server.h"
 
 struct data {
-int csd;
-char id[12];
-struct node *next;
-}n;
-// General macros
-#define MAX 4096
-#define MAX_CMD 20
-#define MAXCONN 1000
+    int csd;
+    char id[12];
+    struct data *next;
+} n;
 
 // Global variables
-int csd=0,csdd=0;
-int socket_write, socket_read,port,portr, ssize,i;
-int csd_all[MAXCONN] = {0};
-int csd_general[MAXCONN] = {0};
-int csd_sb[MAXCONN] = {0};
-long int count_gen = 0;
-long int count_sb = 0;
-static int isGenRunning=0;
-static int isRunning=0;
-pthread_mutex_t lock;
-pthread_t threads,threada;
-fd_set readfds;
+int socket_write;
+pthread_t threads;
 struct data *root=NULL;
-void exit_function(char message[MAX])
-{	
-	printf("Exiting server with error : %s\n",message);
-	exit(0);
+
+void exitFunction(char message[])
+{
+    printf("Exiting server with error : %s\n",message);
+    exit(0);
+}
+
+void broadcastToSwitchBoardAndGetReply(char message[],int *csdd)
+{
+    char reply[MAX_CMD];
+
+    struct data *tempp;
+    tempp=root;
+
+    while(tempp  != NULL)
+    {
+        // Check specific switchboard according to id
+        // through the linked list that
+        if(strstr(message,tempp->id)!= NULL)
+        {
+            // Pass command to switch board
+            write(tempp->csd,message, MAX_CMD);
+
+            // get reply from switchboard
+            read(tempp->csd,reply,MAX_CMD);
+
+            // write back to bridge server.
+            write(*csdd,reply,MAX_CMD);
+            break;
+        }
+        else
+        {
+            tempp=tempp->next;
+        }
+    }
+
+    if(tempp == NULL)
+    {
+        write(*csdd,NOSWITCHBOARD,MAX_CMD);
+    }
 }
 
 
-// Broadcast msg for our switchboard. Strictly 11 chars only.
-void broadcast_Message_For_SB(char message[],int *csdd)
+void *serveRequest( void *param)
 {
-	int i;
-	char reply[20];
+    int *csdd = (int*)param;
+    char message[MAX_CMD];
 
-	struct data *tempp;
-	tempp=root;
-			//printf("start searching our desired board \n");	
-		while(tempp  != NULL)
-		{
-			if(strstr(message,tempp->id)!= NULL)
-			{
-				printf("v1 got it \n");
-				write(tempp->csd,message, strlen(message)+1);
-				printf("sent it\n");
-				read(tempp->csd,reply,20);
-				printf("received reply %s\n",reply);
-				write(*csdd,reply,strlen(reply)+1);
-				printf("transaction over \n");
-				break;
-			}
-			else
-			{
-				tempp=tempp->next;
-			}
-		}
-		if(tempp == NULL)
-		{
-			write(*csdd,"NOSWITCHBOARD",14);
-		}
-//printf("cmd compelted successfully \n");
-}
+    while(1) {
+        if( read(*csdd,message,MAX_CMD) > 0 )
+        {
+            if(strcmp(message,SWITCHBOARD)==0)
+            {
+                // found that new switchboard is connected. We have found
+                // it so acknowledge it.
+                write(*csdd,ACKNOWLEDGE,MAX_CMD);
 
+                // now sb will send its id so store it in linked list.
+                read(*csdd,message,MAX_CMD);
 
-void *start_listen_To_all( void *param)
-{
-	int *csdd = (int*)param;
-	int i = 0;
-	char message[MAX_CMD];
-	
-	while(1){
-		if( read(*csdd,message,20) > 0 )
-		{
-			//printf("Recevied command %s\n",message);
-			if(strcmp(message,"SWITCHBOARD")==0)
-			{
-			
-				//printf("Seems switchboard \n");
-				write(*csdd,"ACKNOWLEDGE",12);
-				read(*csdd,message,13);
-				write(*csdd,"ACKNOWLEDGE",12);
-				
-				struct data *temp;
-				struct data *ptr = (struct data *)malloc(sizeof(struct data));
-				ptr->csd=*csdd;
-				strcpy(ptr->id,message);
+                // acknowledge that we have got id.
+                write(*csdd,ACKNOWLEDGE,MAX_CMD);
 
-				struct data *temppp;
-				temppp=root;
-				
-		while(temppp  != NULL)
-		{
-			if(strstr(message,temppp->id)!= NULL)
-			{
-				close(temppp->csd);
-				temppp->csd = *csdd;
-				free(ptr);
-				break;
-			}
-			else
-			{
-				temppp=temppp->next;
-			}
-		}
-		if(temppp == NULL)
-		{
-			if(root == NULL )
-				{
-					root = ptr;
-					root->next = NULL;
-				}
-				else
-				{
-					temp=root;
-					while(temp->next != NULL)
-					{
-						temp=temp->next;
-					}
-					temp->next=ptr;
-					ptr->next=NULL;
-				}
-		}
-					//printf("Done adding switchboard to database \n");		
-				//pthread_create( &threada, NULL,start_listen_To_SB,(void *)csdd);
-				break;
-			}
-			else if(strcmp(message,"HEARTBEATCK")==0)
-			{
-				write(*csdd,"IAMTHERE4U",12);
-				close(*csdd);
-				break;
-			}	
-			else
-			{
-				//printf("locking mutex \n");
-				pthread_mutex_lock( &lock);
-				//printf("locked mutex and asking board\n");
-				broadcast_Message_For_SB(message,csdd);
-				//printf("done asking board... unlocking mutex \n");
-				pthread_mutex_unlock( &lock);
-				//printf("unlocked mutex \n");
-				close(*csdd);
-				break;
-			}
-		}}
+                // store that id in linked list so that we can communicate
+                // with it lator to pass command.
+                struct data *temp;
+                struct data *ptr = (struct data *)malloc(sizeof(struct data));
+                ptr->csd=*csdd;
+                strcpy(ptr->id,message);
 
+                struct data *temppp;
+                temppp=root;
+
+                while(temppp  != NULL)
+                {
+                    if(strstr(message,temppp->id)!= NULL)
+                    {
+                        close(temppp->csd);
+                        temppp->csd = *csdd;
+                        free(ptr);
+                        break;
+                    }
+                    else
+                    {
+                        temppp = temppp->next;
+                    }
+                }
+                if(temppp == NULL)
+                {
+                    if(root == NULL )
+                    {
+                        root = ptr;
+                        root->next = NULL;
+                    }
+                    else
+                    {
+                        temp=root;
+                        while(temp->next != NULL)
+                        {
+                            temp=temp->next;
+                        }
+                        temp->next=ptr;
+                        ptr->next=NULL;
+                    }
+                }
+
+                break;
+            }
+            else if(strcmp(message,HEARTBEAT)==0)
+            {
+                // it have been asked for heartbeat. reply them that server is here.
+                write(*csdd,IAMTHERE4U,MAX_CMD);
+                close(*csdd);
+                break;
+            }
+            else
+            {
+		// it is not heartbeat or switchboard, so beliving that it is request from
+		// bridgeserver and pass command to perticular switchboard.
+                broadcastToSwitchBoardAndGetReply(message,csdd);
+
+                close(*csdd);
+                break;
+            }
+        }
+    }
 }
 
 
 
 int main(void)
 {
-	
-	long int count=1,i;
-	struct sockaddr_in saddress,raddress;
-	struct sockaddr_in caddress[MAXCONN]; 
-	
-	pthread_mutex_init( &lock, NULL);
-	char message[MAX];
 
-	socket_write = socket( AF_INET, SOCK_STREAM, 0);
-	if( socket_write == -1)
-	{
-		exit_function("Socket init write");
-	}
+    int csd;
+    struct sockaddr_in saddress;
 
-	int on = 1;
-	int ret = setsockopt( socket_write, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on) );
+    // create socket
+    socket_write = socket( AF_INET, SOCK_STREAM, 0);
+    if( socket_write == -1)
+    {
+        exitFunction("Socket init write");
+    }
 
-	/* bind address to socket */
-	saddress.sin_family = AF_INET;
-	saddress.sin_port = htons(5000);
-	saddress.sin_addr.s_addr = inet_addr("192.168.0.1");
+    int on = 1;
+    int ret = setsockopt( socket_write, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on) );
 
-	if( bind( socket_write, (struct sockaddr*)&saddress, sizeof(saddress)) == -1)
-	{
-		exit_function("Socket bind write");
-	}
+    // bind address to socket
+    saddress.sin_family = AF_INET;
+    saddress.sin_port = htons(SERVER_PORT);
+    saddress.sin_addr.s_addr = inet_addr(SERVER_IP);
 
-	/* mark socket to listen for incoming connections */
-	listen( socket_write, MAXCONN);
-	
-	/* wait for a client connection */
-	ssize = sizeof(caddress[0]);
-	while(1)
-	{
-		csd = accept( socket_write, (struct sockaddr*)NULL,NULL);
-		pthread_create( &threads, NULL,start_listen_To_all, (void*)&csd);
-		//printf("Thread started ..\n");
-		pthread_join(threads,NULL);
-		//printf("Thread finished.. \n");
-	}	
-		pthread_exit(NULL);
-	
+    if( bind( socket_write, (struct sockaddr*)&saddress, sizeof(saddress)) == -1)
+    {
+        exitFunction("Socket bind write");
+    }
+
+    // mark socket to listen for incoming connections
+    listen( socket_write, MAXCONN);
+
+    // wait for a client connection
+    while(1)
+    {
+        csd = accept( socket_write, (struct sockaddr*)NULL,NULL);
+        pthread_create( &threads, NULL,serveRequest, (void*)&csd);
+        pthread_join(threads,NULL);
+    }
+
+    pthread_exit(NULL);
+
 }
 
 
---
+++ server.h
@@ -0,0 +1,37 @@
+//General header files
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<netinet/in.h>
+#include<pthread.h>
+#include<string.h>
+#include<signal.h>
+
+// General macros
+#define MAX_CMD 20
+#define MAXCONN 1000
+#define SERVER_IP "192.168.0.1"
+#define SERVER_PORT 5000
+#define NOSWITCHBOARD "NOSWITCHBOARD"
+#define SWITCHBOARD "SWITCHBOARD"
+#define ACKNOWLEDGE "ACKNOWLEDGE"
+#define HEARTBEAT "HEARTBEATCK"
+#define IAMTHERE4U "IAMTHERE4U"
+
+// Quit program in case of error
+// @ input : exit message
+void exitFunction(char[]);
+
+// Broadcast command given by bridge server to perticular switchboard.
+// Tells switch board to perfomr given command and get reply from sb.
+// also pass on that reply to bridge server.
+// @ input : message to be sent and socked id.
+void broadcastToSwitchBoardAndGetReply(char[],int *);
+
+// Serve the request request by mobile app or anything from public.
+// It replies to specific command by sending request to appropriate switch board 
+// and taking reply from it.
+// @ input : cliend socket id
+void *serveRequest( void *);
